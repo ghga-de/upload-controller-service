7f9d7ff0bf0201f701a5fefed83e04d6290f111b
Moritz Hahn <75744178+MoritzHahn1337@users.noreply.github.com>
2022-01-10 15:45:32 +0100

    Request url for upload (GDEV-281)
    Added /health endpoint to indicate if a service is alive
    Added /presigned_post/{file_id} endpoint to return presigned url for uploading files

--- upload_controller_service/core/main.py
+++ upload_controller_service/core/main.py
@@ -19,7 +19,7 @@
 from typing import List
 
 from ..config import CONFIG, Config
-from ..dao import Database
+from ..dao import Database, ObjectStorage
 from ..models import FileInfoInternal
 
 
@@ -31,3 +31,25 @@ def handle_new_study(study_files: List[FileInfoInternal], config: Config = CONFI
     for file in study_files:
         with Database(config=config) as database:
             database.register_file(file)
+
+
+def get_upload_url(file_id: str, config: Config = CONFIG):
+    """
+    Checks if the file_id is in the database, the proceeds to create a presigned
+    post url for an s3 staging bucket
+    """
+
+    # Check if file is in db, will throw an exception if not
+    with Database(config=config) as database:
+        database.get_file(file_id=file_id)
+
+    # Create presigned post for file_id
+    with ObjectStorage(config=config) as storage:
+        if not storage.does_bucket_exist(bucket_id=config.inbox_bucket_name):
+            storage.create_bucket(config.inbox_bucket_name)
+
+        presigned_post = storage.get_object_upload_url(
+            bucket_id=config.inbox_bucket_name, object_id=file_id
+        )
+
+    return presigned_post
