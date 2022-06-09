91e4f2770f9b9530ba90eaa423cbc5a2beece6a5
Kersten Breuer <kersten-breuer@outlook.com>
2022-06-09 17:21:31 +0200

    adapt API to allow for multi-part upload (GDEV-754) (#21)
    This PR just contain stubs of FastAPI view functions to
    that shall recreate the spec agreed upon in:
    https://github.com/ghga-de/epic-docs/pull/27
    
    Implementation will be realized in following PRs.
    Until then, some tests remain xfailed.
    
    Co-authored-by: Moritz Hahn <75744178+MoritzHahn1337@users.noreply.github.com>
    Co-authored-by: Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>

---
+++ ucs/adapters/inbound/fastapi_/models.py
@@ -0,0 +1,90 @@
+# Copyright 2021 - 2022 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""REST API-specific data models (not used by core package)"""
+
+from datetime import datetime
+from typing import Literal, Optional
+
+from pydantic import BaseModel, Field
+
+
+class FileMetadata(BaseModel):
+    """
+    A model containing all the metadata needed to pass it on to other microservices
+    """
+
+    file_id: str
+    file_name: str
+    md5_checksum: str
+    size: int
+    grouping_label: str
+    creation_date: datetime
+    update_date: datetime
+    format: str
+    current_upload_id: Optional[str] = Field(
+        None,
+        description="ID of the current upload. `Null` if no update has been initiated, yet.",
+    )
+
+
+class UploadCreation(BaseModel):
+    """Properties required to create a new upload."""
+
+    file_id: str = Field(
+        ..., description="The ID of the file corresponding to this upload."
+    )
+
+    class Config:
+        """Additional Model Config."""
+
+        title = "Properties required to create a new upload"
+
+
+class UploadDetails(UploadCreation):
+    """Details returned upon creation of a new multipart upload."""
+
+    upload_id: str
+    part_size: int = Field(
+        ..., description="Part size to be used for upload. Specified in bytes."
+    )
+
+    class Config:
+        """Additional Model Config."""
+
+        title = "Multi-Part Upload Details"
+
+
+class UploadUpdate(BaseModel):
+    """Request body to update an existing mutli-part upload."""
+
+    status: Literal["uploaded", "cancelled"]
+
+    class Config:
+        """Additional Model Config."""
+
+        title = "Multi-Part Upload Update"
+
+
+class AccessURL(BaseModel):
+    """Contains a pre-signed for uploading the bytes of one file part."""
+
+    url: str = Field(
+        ...,
+        description=(
+            "A fully resolvable URL that can be used to upload the actual"
+            + " object bytes for one upload part."
+        ),
+    )
---
+++ ucs/adapters/inbound/fastapi_/routes.py
@@ -0,0 +1,200 @@
+# Copyright 2021 - 2022 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module containing the main FastAPI router and all route functions.
+"""
+
+from fastapi import APIRouter, HTTPException, Path, status
+
+from ucs.adapters.inbound.fastapi_.models import (
+    AccessURL,
+    FileMetadata,
+    UploadCreation,
+    UploadDetails,
+    UploadUpdate,
+)
+
+router = APIRouter()
+
+
+class HttpFileNotFoundException(HTTPException):
+    """Thrown when a file with given ID could not be found."""
+
+    def __init__(self, file_id: str):
+        """Construct message and init the exception."""
+        super().__init__(
+            status_code=404,
+            detail=f'The file with the file_id "{file_id}" does not exist.',
+        )
+
+
+@router.get("/health", summary="health", status_code=status.HTTP_200_OK)
+def health():
+    """Used to test if this service is alive"""
+    return {"status": "OK"}
+
+
+@router.get(
+    "/files/{file_id}",
+    summary="Get file metadata including the current upload attempt.",
+    operation_id="getFileMetadata",
+    status_code=status.HTTP_200_OK,
+    response_model=FileMetadata,
+    response_description="File metadata including the current upload attempt",
+    responses={
+        status.HTTP_403_FORBIDDEN: {
+            "description": (
+                "The user is not registered as a Data Submitter for the corresponding file."
+            )
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": (
+                "The file with the given ID has not (yet) been registered for upload."
+            )
+        },
+    },
+)
+def get_file_metadata(
+    file_id: str,
+):
+    """Get file metadata including the current upload attempt."""
+
+    print(file_id)
+    ...
+
+    return ...
+
+
+@router.post(
+    "uploads",
+    summary="Initiate a new multi-part upload.",
+    operation_id="createUpload",
+    response_model=UploadDetails,
+    status_code=status.HTTP_200_OK,
+    response_description="Details on the newly created upload.",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": (
+                "It is currently not possible to create a new upload for the file with"
+                + " the specified ID because another upload for that file is already"
+                + ' active or has been accepted (its status is not "failed"'
+                + ' "cancelled", or "rejected").',
+            )
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": (
+                "The user is not registered as a Data Submitter for the corresponding"
+                + " file."
+            )
+        },
+    },
+)
+def create_upload(upload_creation: UploadCreation):
+    """Initiate a new mutli-part upload for the given file."""
+
+    print(upload_creation)
+    ...
+
+    return ...
+
+
+@router.get(
+    "/uploads/{upload_id}",
+    summary="Get details on a specific upload.",
+    operation_id="getUploadDetails",
+    status_code=status.HTTP_200_OK,
+    response_model=UploadDetails,
+    response_description="Details on a specific upload.",
+    responses={
+        status.HTTP_403_FORBIDDEN: {
+            "description": (
+                "The user is not registered as a Data Submitter for the file"
+                + " corresponding to this multi-part upload."
+            )
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The multi-part upload with the given ID does not exist."
+        },
+    },
+)
+def get_upload_details(upload_id: str):
+    """
+    Get details on a specific upload.
+    """
+    print(upload_id)
+    ...
+
+
+@router.patch(
+    "/uploads/{upload_id}",
+    summary="Update the status of an existing multi-part upload.",
+    operation_id="updateUploadStatus",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_description="Multi-part upload successfully updated.",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": (
+                "The provided status value was invalid or the status of this multi-part"
+                + " upload currently or permanently can't be changed."
+            )
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": (
+                "The user is not registered as a Data Submitter for the file"
+                + " corresponding to this multi-part upload."
+            )
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The multi-part upload with the given ID does not exist."
+        },
+    },
+)
+def update_upload_status(upload_id: str, update: UploadUpdate):
+    """
+    Declare a multi-part upload as complete by setting its status to "uploaded".
+    Or cancel a multi-part upload by setting its status to "cancelled".
+    """
+    print(upload_id, update)
+    ...
+
+
+@router.post(
+    "/uploads/{upload_id}/parts/{part_no}/signed_urls",
+    summary="Create new pre-signed URL for a specific part.",
+    operation_id="createPreSignedURL",
+    status_code=status.HTTP_200_OK,
+    response_model=AccessURL,
+    response_description="The newly created pre-signed URL.",
+    responses={
+        status.HTTP_403_FORBIDDEN: {
+            "description": (
+                "The user is not registered as a Data Submitter for the file"
+                + " corresponding to this multi-part upload."
+            )
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The multi-part upload with the given ID does not exist."
+        },
+    },
+)
+def create_presigned_url(upload_id: str, part_no: int = Path(..., ge=1, le=10000)):
+    """
+    Create a pre-signed URL for the specified part number of the specified multi-part
+    upload.
+    """
+
+    print(upload_id, part_no)
+    ...
